6e974a2ad38a3ccf25c2e1a854f6233f2be93962
thanhtupppp <devthanhtu@gmail.com>
2026-09-13 08:23:41 +0700

    feat: add Claymorphism Flutter implementation seed

---
+++ skills/claymorphism/example.flutter.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ClayCard extends StatelessWidget {
+  const ClayCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: const Color(0xFFCFD4FF),
+        borderRadius: BorderRadius.circular(32),
+        boxShadow: const [
+          BoxShadow(color: Color(0x3E52425B), offset: Offset(0, 18), blurRadius: 28),
+          BoxShadow(color: Color(0x80FFFFFF), offset: Offset(0, -5), blurRadius: 12),
+        ],
+      ),
+      child: FilledButton(onPressed: () {}, child: const Text('Start')),
+    );
+  }
+}
